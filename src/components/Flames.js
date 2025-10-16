3289679aefc4894798afc02364b3e8794f7d29bf
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-16 19:29:37 +0530

    Change 4

--- src/components/Flames.js
+++ src/components/Flames.js
@@ -1,4 +1,21 @@
-function RelationShip() {
+import React, { useState } from 'react';
+
+function Flames() {
+  const [relationType, setRelationType] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+
+  const RelationShipStatus = {
+    1: 'Friends',
+    2: 'Love',
+    3: 'Affection',
+    4: 'Marriage',
+    5: 'Enemy',
+    0: 'Siblings',
+    Blank_Input: 'Please Enter valid input',
+  };
+
+  function RelationShip() {
   if (firstName.trim() === '' || secondName.trim() === '') {
     setRelationType(RelationShipStatus.Blank_Input);
     return;
@@ -31,3 +48,47 @@ function RelationShip() {
   const status = RelationShipStatus[relationShipCode];
   setRelationType(status);
 }
+
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        data-testid="input1"
+        name="name1"
+      />
+      <input
+        type="text"
+        placeholder="Second Name"
+        value={secondName}
+        onChange={(e) => setSecondName(e.target.value)}
+        data-testid="input2"
+        name="name2"
+      />
+      <button
+        onClick={RelationShip}
+        data-testid="calculate_relationship"
+        name="calculate_relationship"
+      >
+        Calculate RelationShip Future
+      </button>
+      <button
+        onClick={() => {
+          setFirstName('');
+          setSecondName('');
+          setRelationType('');
+        }}
+        data-testid="clear"
+        name="clear"
+      >
+        Clear
+      </button>
+      <h3 data-testid="answer">{relationType}</h3>
+    </>
+  );
+}
+
+export default Flames;
